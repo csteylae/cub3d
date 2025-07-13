2df5db384e423d0ee44cd7e65b0050e5f3731a73
Rachel Neumann <raneuman@mi-r3-p3.s19.be>
2025-07-13 15:36:11 +0200

    fixed txts path error

--- src/input_parsing/parsing/textures/textures_2.c
+++ src/input_parsing/parsing/textures/textures_2.c
@@ -6,7 +6,7 @@
 /*   By: raneuman <raneuman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 18:01:39 by raneuman          #+#    #+#             */
-/*   Updated: 2025/07/08 22:04:27 by raneuman         ###   ########.fr       */
+/*   Updated: 2025/07/13 15:34:44 by raneuman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void	check_textures_path(char *path, t_data *game)
 	if (fd == -1)
 		free_all_error("Error\nInvalid file!\n", game);
 	len = ft_strlen(path);
-	if ((ft_strncmp(path + len - 4, ".xpm", 4)) != 0)
+	if (len < 4 || (ft_strncmp(path + len - 4, ".xpm", 4)) != 0)
 		free_all_error("Error\nInvalid file!\n", game);
 }
